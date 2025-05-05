43eea76b336694b6c784b010e9be42d86c0b6a3e
KadakT <49987536+KadakT@users.noreply.github.com>
2025-05-05 16:23:08 +0300

    working on responsive views

--- server.js
+++ server.js
@@ -12,16 +12,25 @@ app.use(
     directives: {
       defaultSrc: ["'self'"],
       scriptSrc: ["'self'"],
-      styleSrc: ["'self'", "'unsafe-inline'"], 
+      styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
       imgSrc: ["'self'", "https:"],           
-      fontSrc: ["'self'", "https:"],
+      fontSrc: ["'self'", "https://fonts.gstatic.com"],
       connectSrc: ["'self'"],
     },
   })
 );
 
 // Static files
-app.use(express.static(path.join(__dirname, 'public')));
+const oneMonth = 1000 * 60 * 60 * 24 * 30; 
+
+app.use(express.static(path.join(__dirname, 'public'), {
+  maxAge: oneMonth, 
+  setHeaders: function (res, path) {
+    if (path.endsWith('.html') || path.endsWith('.js') || path.endsWith('.css')) {
+      res.setHeader('Cache-Control', 'no-cache');
+    }
+  }
+}));
 
 // Serve index.html
 app.get('/', (req, res) => {
